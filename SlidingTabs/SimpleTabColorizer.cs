04dd59edc3d328237965ba101fc1c0fc1ca0b0d6
DerekRivers <dyk3rz@googlemail.com>
2017-01-18 12:15:29 +0000

    Added ScrolltabView

---
+++ SlidingTabs/SimpleTabColorizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SlidingTabs
+{
+    public class SimpleTabColorizer : SlidingTabScrollView.TabColorizer
+    {
+        private int[] mIndicatorColors;
+        private int[] mDivideColors;
+
+        public int GetIndicatorColor(int position)
+        {
+            return mIndicatorColors[position % mIndicatorColors.Length];
+        }
+
+        public int GetDividerColors(int position)
+        {
+            return mDivideColors[position % mDivideColors.Length];
+        }
+
+        public int[] IndicatorColors
+        {
+            set { mIndicatorColors = value; }
+        }
+
+        public int[] DividerColors
+        {
+            set { mDivideColors = value; }
+        }
+    }
+}
